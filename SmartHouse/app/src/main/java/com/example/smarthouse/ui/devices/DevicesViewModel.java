cc02b64167f4620be625700e744d32a51c667c00
lykhtar <lykh-anastasiya@yandex.ru>
2021-11-19 14:57:11 +0100

    Create DevicesViewModel.java

---
+++ SmartHouse/app/src/main/java/com/example/smarthouse/ui/devices/DevicesViewModel.java
@@ -0,0 +1,18 @@
+package com.example.smarthouse.ui.devices;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class DevicesViewModel extends ViewModel {
+
+    private MutableLiveData<String> mText;
+
+    public DevicesViewModel() {
+        mText = new MutableLiveData<>();
+    }
+
+    public LiveData<String> getText() {
+        return mText;
+    }
+}
